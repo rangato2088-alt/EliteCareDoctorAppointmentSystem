e3b10c0e33d20135fbe8913145d5a4e7c1d3be35
rangato2088-alt <rangato2088@gmail.com>
2026-06-02 17:49:57 +0530

    Implemented login authentication module

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -3,11 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package dao;
+import db.DBConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.User;
 
-/**
- *
- * @author user
- */
 public class UserDAO {
+    public User login(String username, String password) {
+
+        User user = null;
+
+        try {
+
+            Connection con = DBConnection.getConnection();
+
+            String sql = "SELECT * FROM users WHERE username=? AND password=? AND status='ACTIVE'";
+
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+
+                user = new User();
+
+                user.setUserId(rs.getInt("user_id"));
+                user.setUsername(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                user.setRole(rs.getString("role"));
+                user.setStatus(rs.getString("status"));
+            }
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+
+        return user;
+    }
     
 }
--- src/model/User.java
+++ src/model/User.java
@@ -9,5 +9,49 @@ package model;
  * @author user
  */
 public class User {
-    
+    private int userId;
+    private String username;
+    private String password;
+    private String role;
+    private String status;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
